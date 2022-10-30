01422cbeeca8e30e94eede286f6cbb63c1a85e6d
unknown <bacigalupo996@gmail.com>
2022-10-30 18:40:03 -0400

    updated tb

---
+++ ALU_Workspace/ALU/hdl_assist/hdl_assist.cmd
@@ -0,0 +1,1 @@
+ -w -cvi  -2002  -s "B:/Stony Brook/ESE345/4_stage_multi/ALU_Workspace/ALU/hdl_assist/library.cfg"  -work ALU  -e 100  "B:/Stony Brook/ESE345/4_stage_multi/ALU_Workspace/ALU/hdl_assist/2946-9502-ea50-cb5e"
