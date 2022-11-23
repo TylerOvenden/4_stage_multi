85593a0f3efaafa876acd2e830614582be91139a
TylerOvenden <tovenden4386@bths.edu>
2022-11-22 22:06:42 -0500

    instruction_buff testbench

--- instruction_buffer.vhd
+++ instruction_buffer.vhd
@@ -19,32 +19,50 @@
 
 
 --Using this to build array of instr
---https://stackoverflow.com/questions/30651269/synthesizable-multidimensional-arrays-in-vhdl
+--https://stackoverflow.com/questions/30651269/synthesizable-multidimensional-arrays-in-vhdl	 
+
+library IEEE;
+use IEEE.std_logic_1164.all;	 
+use ieee.numeric_std.all;
+
+
+package instr_pack is 
+	type InstBuff is array (0 to 63) of std_logic_vector(24 downto 0);	
+end package;
+
+
+
+
+
 library ieee;
 use ieee.std_logic_1164.all;  
 use ieee.numeric_std.all;
 use ieee.std_logic_unsigned.all;	
+use work.instr_pack.all;
+
 
 
 entity instruction_buffer is
-generic (			 
-n : integer := 6; -- 2^6 = 64 values for pc
-buffer_size : integer := 64;	 
-width : integer := 2;	
-inst_size : integer := 25
---subtype instr is std_logic_vector(24 downto 0);
---type instr_buffer is array (0 to 63 ) of instr; 
-
-
-);
+--generic (	
+--in_buffer: in InstBuff;
+--n : integer := 6; -- 2^6 = 64 values for pc
+--buffer_size : integer := 64;	 
+--width : integer := 2;	
+--inst_size : integer := 25
+----subtype instr is std_logic_vector(24 downto 0);
+----type instr_buffer is array (0 to 63 ) of instr; 
+--
+--
+--);
 port (
 clk : in std_logic; -- system clock
 clr : in std_logic; -- synchrounous counter clear		  
 write: in std_logic;	--write signal	   
-
+ in_buffer: in InstBuff;
 --pc : in std_logic_vector(n-1 downto 0);
-instr_in : in std_logic_vector(inst_size-1 downto 0);
-instr_out : out std_logic_vector(inst_size-1 downto 0)
+instr_in : in std_logic_vector(24 downto 0);	
+
+instr_out : out std_logic_vector(24 downto 0)
 --pc : out std_logic_vector(n-1 downto 0)
 );
 
@@ -53,9 +71,9 @@ end instruction_buffer;
 			 
 architecture behaviorhal of instruction_buffer is	
 
-type InstBuff is array (0 to buffer_size-1) of std_logic_vector(24 downto 0);
+--type InstBuff is array (0 to buffer_size-1) of std_logic_vector(24 downto 0);
 
-signal ins : InstBuff;
+--signal ins : InstBuff;
 
 --variable temp : integer;
 begin
@@ -70,10 +88,13 @@ if rising_edge(clk)		 then
 						  
 	
 	
-	instr_out <= ins(pc);	  
-	--
+	instr_out <= in_buffer(pc);	  
+	--			
+	if(pc = 63) then  
+		pc := 0;
+	else 		
 	pc := pc + 1;
-	
+		 end if;
 end if;
  --pc := std_logic_vector(to_unsigned(count));
 end process;						   
@@ -103,3 +124,4 @@ begin
 		end if;
 	end process;
 end behavioral;
+	
--- instruction_buffer_tb.vhd
+++ instruction_buffer_tb.vhd
@@ -3,22 +3,27 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use STD.textio.all;
-use ieee.std_logic_textio.all;
+use ieee.std_logic_textio.all; 
+
 
 
 use std.textio.all;
 
+library work;
 use work.all;
+use work.instr_pack.all;
+
 
 entity instruction_buffer_tb is
-generic(
-n : integer := 6 ;	
-width : integer := 2 ;	
-inst_size : integer := 25 ;	
--- width of ADC
-		-- integer analog value simulated, Vin = (analog_val * Vref)/2**n
-		buffer_size : integer := 64);		
-	
+--generic(
+--n : integer := 6 ;	
+--width : integer := 2 ;	
+--inst_size : integer := 25 ;	
+--
+---- width of ADC
+--		-- integer analog value simulated, Vin = (analog_val * Vref)/2**n
+--		buffer_size : integer := 64);		
+--	
 	
 end instruction_buffer_tb;
 
@@ -29,61 +34,76 @@ architecture tb of instruction_buffer_tb is
 	file txt_file : text;
 
 
-
+	signal in_buffer: InstBuff;
 	signal clk, clr, write: std_logic;
 
 	--signal cmptr : std_logic;									 
-	signal instr_out, instr_in : std_logic_vector(inst_size-1 downto 0);
+	signal instr_out, instr_in : std_logic_vector(24 downto 0);
 	constant period : time := 5us;				 
-	
+--	type InstBuff is array (0 to 63) of std_logic_vector(24 downto 0);	
 	
 	
 begin
 	UUT : entity instruction_buffer	
 		
+	--	
+--		 	generic map (
+--		n => n,
+--		width  =>	 width,
+--		inst_size  =>	  inst_size,
+--		--InstBuff => InstBuff,
+--		buffer_size => buffer_size
+--		)
 		
-		 	generic map (
-		n => n,
-		width  =>	 width,
-		inst_size  =>	  inst_size,
-		buffer_size => buffer_size
-		)
+		port map (in_buffer => in_buffer, clk => clk, clr=> clr, write => write, instr_out => instr_out, instr_in => instr_in);	
+  
+		process 	 
 		
-		port map (clk => clk, clr=> clr, write => write, instr_out => instr_out, instr_in => instr_in);	
-
-		process 
-		variable v_ILINE     : line;
-		variable v_OLINE     : line;
-		variable temp_inst : std_logic_vector(inst_size-1 downto 0);
+		variable lin     : line;
+		--variable v_OLINE     : line;
+		variable temp_inst : std_logic_vector(24 downto 0);
 		
-		variable counter : integer range 0 to 63 := 0;
-		  
+		variable counter : integer := 0;
+		variable ran : boolean := false;  
 
 		  
 		
 		begin	
 
-			
-		      file_open(txt_file, "test.txt", write_mode);
-			--file_open(file_VECTORS, "binary.txt",  read_mode);
-			while not endfile(txt_file) loop
+				if( ran = false) then 
+		    --  file_open(txt_file, "binary.txt", write_mode);
+			file_open(txt_file, "binary.txt",  read_mode);
+			while ( not endfile(txt_file) and (counter <64 ) )loop
 		
-				readline(txt_file, v_ILINE);
-				read(v_ILINE, temp_inst);
-   		
-				instr_in <= temp_inst;   
+				readline(txt_file, lin);
+				read(lin, temp_inst);
+   				in_buffer(counter) <=  temp_inst;
+				--instr_in <= temp_inst;   
+				
+				
+				
 				counter := counter + 1;
-				
-				
-				 wait for period;
-			end loop;
-			
-			
-			
-			
-			
 
-	end process;	  
+				
+			end loop; 		  
+			counter	:= 0;
+			ran := true;
+    	file_close(txt_file);	
+		  end if;
+	--	std.env.finish;
+		wait; 
+		end process;
+		
+		
+		 clock: process
+  begin 
+	  clk <='0';
+	  wait for period;
+	  clk<='1';
+	  wait for period;
+  end process;
+			
+			
 	
 end tb;
 			 	
